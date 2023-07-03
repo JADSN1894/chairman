e4ad6875e65f371c5b1dd5f42002811ccfb098ee
jadsn <120682244+JADSN1894@users.noreply.github.com>
2023-07-03 19:18:46 -0300

    feat(copy): Implement copy

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,19 @@ import preprocess from "svelte-preprocess";
 import sveltePreprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		version: {
+			name: pkg.version
+		},
 		adapter: adapter(
 			{
 				// default options are shown
@@ -21,7 +31,8 @@ const config = {
 			'$stores': './src/stores',
 			'$types': './src/types',
 			'$components': './src/components',
-			'$i18n': './src/i18n'
+			'$i18n': './src/i18n',
+			'$icons': './src/icons'
 		},
 		csp: {
 			mode: "hash",
